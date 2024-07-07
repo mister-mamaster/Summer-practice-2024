646da7907eba170ce903d063a8a01dbe321cc63a
Alexey Burlak <burlakop@gmail.com>
2024-07-07 22:14:37 +0400

    Authorization added (config)

--- src/main/java/ru/opencode/bankinfo/config/SecurityConfig.java
+++ src/main/java/ru/opencode/bankinfo/config/SecurityConfig.java
@@ -1,37 +1,41 @@
-//package ru.opencode.bankinfo.core.exception.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http.csrf().disable()
-//                .headers(headers -> headers.frameOptions().sameOrigin())
-//                .authorizeHttpRequests(requests -> requests
-////                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-//                        .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).hasRole("ADMIN")
-//                        .anyRequest().authenticated()
-//                )
-//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .httpBasic(Customizer.withDefaults())
-//                .build();
-//    }
-//}
+package ru.opencode.bankinfo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.opencode.bankinfo.security.entity.User;
+import ru.opencode.bankinfo.security.repository.UserRepository;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return (username) -> {
+            User user = userRepo.findByLogin(username);
+            if(user != null) return user;
+
+            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(Customizer.withDefaults());
+        return http
+                .authorizeHttpRequests(
+                        request -> request.requestMatchers("/", "/**").hasRole("USER")
+                )
+                .build();
+    }
+}
